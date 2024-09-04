28c314234c0799f313c7badf2f0b170058400f5b
gestskoli <ges@tskoli.is>
2024-09-04 11:20:12 +0000

    Create flokkur.rs

---
+++ tímar/klasar1/src/flokkur.rs
@@ -0,0 +1,33 @@
+use std::fmt::Display;
+
+#[derive(Debug)]
+pub enum Flokkur {
+    Folksbill,
+    Jeppi,
+    Vorubill,
+    Annad,
+}
+
+impl Display for Flokkur {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let flokkur = match self {
+            Flokkur::Folksbill => "Fólksbíll",
+            Flokkur::Jeppi => "Jeppi",
+            Flokkur::Vorubill => "Vörubíll",
+            Flokkur::Annad => "Annað",
+        };
+        write!(f, "{}", flokkur)
+    }
+}
+
+// value verður jeppi, JEPPI, Jeppi, fólksbíll, sjdlfkas fl
+impl From<&str> for Flokkur {
+    fn from(value: &str) -> Self {
+        match value.to_lowercase().as_str() {
+            "fólksbíll" => Flokkur::Folksbill,
+            "jeppi" => Flokkur::Jeppi,
+            "vörubíll" => Flokkur::Vorubill,
+            _ => Flokkur::Annad,
+        }
+    }
+}
