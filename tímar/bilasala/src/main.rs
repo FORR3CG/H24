7dfba66043ae0fb29b0defb9348fb0fe78105ed8
gestskoli <ges@tskoli.is>
2024-09-17 13:32:25 +0000

    bílasala byrjað

--- tímar/bilasala/src/main.rs
+++ tímar/bilasala/src/main.rs
@@ -51,6 +51,53 @@ impl Display for Bill {
     }
 }
 
+struct Bilasala {
+    bilar: Vec<Bill>,
+    id: u32,
+}
+
+impl Bilasala {
+    fn new() -> Self {
+        Self {
+            bilar: Vec::new(),
+            id: 100,
+        }
+    }
+
+    fn next_id(&mut self) -> u32 {
+        self.id += 1;
+        self.id
+    }
+
+    fn skra(&mut self, framleidandi: &str, gerd: &str, verd: u32) {
+        let id = self.next_id(); // id er afritað (copy)
+        // búinn að skila self (lesaðgangi)
+        self.bilar.push(Bill::new(id, framleidandi, Gerd::from(gerd), verd));
+        //self.bilar.push(Bill::new(self.next_id(), framleidandi, Gerd::from(gerd), verd));
+    }
+
+    fn skra2(&mut self, framleidandi: &str, gerd: &str, verd: u32) {
+        let b = Bill::new(self.next_id(), framleidandi, Gerd::from(gerd), verd);
+        self.bilar.push(b);
+    }
+}
+
+impl Display for Bilasala {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut bilar_texti = String::new();
+        for bill in &self.bilar {
+            bilar_texti.push_str(format!("{}\n", bill).as_str());
+        }
+        write!(f, "{}", bilar_texti)
+    }
+}
+
 fn main() {
-   
+    let mut bs = Bilasala::new();
+    bs.skra("toyota", "jeppi", 100000);
+    bs.skra2("Volvo", "fb", 200000);
+    
+    loop {
+        
+    }
 }
