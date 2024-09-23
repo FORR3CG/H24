4a09a307b01a9f54a2f5bb27a7eef5870db8c3ec
gestskoli <ges@tskoli.is>
2024-09-23 16:05:27 +0000

    Ord, PartialOrd, PartialEq

--- tímar/traits/src/main.rs
+++ tímar/traits/src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::Reverse;
+
 
 fn sidasta_i32(fylki: &[i32]) -> &i32 {
     fylki.last().unwrap()
@@ -11,12 +13,7 @@ fn sidasta<T>(fylki: &[T]) -> &T { // T, U, V, W
     fylki.last().unwrap()
 }
 
-#[derive(Debug)]
-struct Bill {
-    tegund: String,
-    litur: String,
-    verd: u32,
-}
+
 
 impl Bill {
     fn new(tegund: &str, litur: &str, verd: u32) -> Self {
@@ -45,13 +42,65 @@ impl TryFrom<&str> for Bill {
     }
 }
 
+#[derive(Debug, Eq)]
+struct Bill {
+    tegund: String,
+    litur: String,
+    verd: u32,
+}
+
+impl Ord for Bill {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        if self.tegund == other.tegund {
+            self.verd.cmp(&other.verd)
+        } else {
+            self.tegund.cmp(&other.tegund)
+        }
+    }
+}
+
+impl PartialOrd for Bill {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Bill {
+    fn eq(&self, other: &Self) -> bool {
+        self.tegund == other.tegund
+    }
+}
+
 fn main() {
-    let bmw = Bill::try_from("bmw hvítur geir");
+    let mut bilar: Vec<Bill> = Vec::new();
+    let audi = Bill::try_from("audi gulur 10000").unwrap();
+    let toyota = Bill::try_from("audi hvítur 500").unwrap();
+    if audi == toyota {
+        // hvenær á þetta að vera true???
+    }
+
+    bilar.push(audi);
+    bilar.push(toyota);
+/*     if audi > toyota {
+        println!("audi er stærri");
+    } else {
+        println!("toyota er stærri")
+    } */
+
+    let bmw = Bill::try_from("bmw hvítur 1000");
     match bmw {
-        Ok(bill) => println!("{:?}", bill),
+        Ok(bill) => bilar.push(bill),
         Err(e) => println!("{}", e),
     }
-   
+
+    if let Ok(bill) = Bill::try_from("audi rauður 1200") {
+        bilar.push(bill);
+    } else {
+        println!("Einhver villa! Reyndu aftur");
+    }
+    println!("fyrir röðun: {:#?}", bilar);
+    bilar.sort();
+    println!("eftir röðun: {:#?}", bilar);
 
     let i32fylki = [1,2,3,4,5];
     let f64fylki = [1.1, 1.2, 1.3];
